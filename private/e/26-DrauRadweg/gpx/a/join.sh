9d977747e235e9d88de5d9585af2456d49b4cfd0
Sandor BERCZI <sandor.berczi@r-software.at>
2026-02-13 15:43:53 +0100

    rc

--- private/e/26-DrauRadweg/gpx/a/join.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python3 /c/Entwicklung/work/_git/dailyUtils/private/gpx/joinGpx/join_gpx.py \
-    ../1-Misurina-Lienz.gpx \
-    ../2-Lienz-Spittal-an-der-Drau.gpx \
-    ../3-Spittal-an-der-Drau-Ferlach.gpx \
-    ../4-Ferlach-Lavamund.gpx \
-    ../5-Lavamund-Maribor.gpx \
-    >0-Misurina-Maribor.gpx
